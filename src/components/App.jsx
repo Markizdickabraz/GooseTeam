58949cfdec4f663fe0b5978984d8ca9386ace0a6
Sakhatskyi Mark <110189603+Markizdickabraz@users.noreply.github.com>
2023-06-21 23:03:49 +0300

    routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,49 +1,46 @@
-import { lazy, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-
-import { selectIsRefreshing } from '../redux/authorization/selectors';
-import { refreshUser } from '../redux/authorization/operations';
+import { lazy,  Suspense } from 'react';
 
 import { Routes, Route } from 'react-router-dom';
 
-// import  Layout  from './Layout/Layout';
+import Month from 'components/calendar/Month';
+import Day from 'components/calendar/Day';
+
+
 
 const Start = lazy(() => import('../pages/Start'));
 const Register = lazy(() => import('../pages/Register'));
 const Login = lazy(() => import('../pages/Login'));
-const Layout = lazy(() => import('./Layout/Layout'));
 const User = lazy(() => import('../pages/User'));
 const Statistics = lazy(() => import('../pages/Statistics'));
 const Calendar = lazy(() => import('../pages/Calendar'));
-const Day = lazy(() => import('../pages/Day'));
 const NotFound = lazy(() => import('../pages/NotFound'));
 
 const App = () => {
-  const dispatch = useDispatch();
-  const isRefreshing = useSelector(selectIsRefreshing);
 
-  useEffect(() => {
-    dispatch(refreshUser());
-  }, [dispatch]);
-
-  return isRefreshing ? (
-    <b>Loading...</b>
-  ) : (
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
     <Routes>
       <Route path="/" element={<Start />} />
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
-
-      <Route path="/" element={<Layout />}>
-        <Route path="account" element={<User />} />
+        <Route
+            path="/account"
+            element={
+              <Suspense fallback={<div>Loading...</div>}>
+                <User />
+              </Suspense>
+            }
+          />
         <Route path="statistics" element={<Statistics />} />
-        {/* <Route path="calendar" element={<Calendar />} /> */}
-        <Route path="calendar/month/:currentDate" element={<Calendar />} />
+        <Route path="calendar" element={<Calendar />}>
+        <Route path="calendar/month/:currentDate" element={<Month />} />
         <Route path="calendar/day/:currentDay" element={<Day />} />
       </Route>
 
       <Route path="*" element={<NotFound />} />
     </Routes>
+
+    </Suspense>
   );
 };
 
---
+++ src/components/calendar/Month.jsx
@@ -0,0 +1,5 @@
+const Month = () => {
+    return <></>;
+  };
+  
+  export default Month;
