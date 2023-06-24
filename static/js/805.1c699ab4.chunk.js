304852e33c831996b1f95bf721b8b31e0def540a
Markizdickabraz <110189603+Markizdickabraz@users.noreply.github.com>
2023-06-24 19:01:55 +0000

    Deploying to gh-pages from @ Markizdickabraz/GooseTeam@04f06c0523b450de2b9e23dc5f6459c1a0eef2c0 🚀

---
+++ static/js/805.1c699ab4.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[805],{9805:function(e,t,r){r.r(t);var a=r(3329);t.default=function(){return(0,a.jsx)(a.Fragment,{})}}}]);
+//# sourceMappingURL=805.1c699ab4.chunk.js.map
