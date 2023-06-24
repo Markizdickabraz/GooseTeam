05da14ae3a239c11cce496d8c871894c7c4951c8
Zefristype <Rojdion.a@gmail.com>
2023-06-24 23:51:37 +0200

    updated routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,14 @@ import 'modern-normalize';
 import { lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
-import ChoosedMonth from 'pages/CalendarPage/ChoosedMonth';
-import ChoosedDay from 'pages/CalendarPage/ChoosedDay';
-
 const Start = lazy(() => import('../pages/Start'));
 const Register = lazy(() => import('../pages/Register'));
 const Login = lazy(() => import('../pages/Login'));
 const User = lazy(() => import('../pages/User'));
 const Statistics = lazy(() => import('../pages/Statistics'));
 const CalendarPage = lazy(() => import('../pages/CalendarPage/CalendarPage'));
+const ChoosedMonth = lazy(() => import('../pages/CalendarPage/ChoosedMonth'));
+const ChoosedDay = lazy(() => import('../pages/CalendarPage/ChoosedDay'));
 const NotFound = lazy(() => import('../pages/NotFound'));
 
 const App = () => {
