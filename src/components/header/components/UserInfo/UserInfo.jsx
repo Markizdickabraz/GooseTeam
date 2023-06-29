743d57546e70dea5b93241fade3c4d46d8f77bb6
Maksym-Plutenko <plutenkom@gmail.com>
2023-06-29 15:55:24 +0300

    back old UserInfo

--- src/components/header/components/UserInfo/UserInfo.jsx
+++ src/components/header/components/UserInfo/UserInfo.jsx
@@ -5,20 +5,10 @@ import {
   UserNameIcon,
   BackgroundName,
 } from './UserInfo.styled';
-// import { useAuth } from 'hooks/useAuth';
-import { useSelector } from 'react-redux';
-import {selectUser} from '../../../../redux/authorization/selectors';
-
+import { useAuth } from 'hooks/useAuth';
 
 export const UserInfo = () => {
-  // const { name, avatarURL } = useAuth();
-
-const user = useSelector(selectUser);
-const name = user.name;
-const avatarURL = user.avatarURL;
-
-// const name = "Агент №86 Максвелл Кмітливий";
-// const avatarURL = 'fastly.picsum.photos/id/596/200/200.jpg?hmac=TiMsstBNF6YKq9Gn7QGsihITEUcv_O8QuTXEVR3T6GA';
+  const { name, avatarURL } = useAuth();
 
   const displayName = avatarURL ? (
     <img src={`https://${avatarURL}`} alt="UserPicture" />
