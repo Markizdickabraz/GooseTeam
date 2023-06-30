f2c41ea968e8466748235c273e1137f58fc9f852
Oleksandr <107481048+RaccoonBLR@users.noreply.github.com>
2023-06-30 23:09:43 +0300

    updated tasks operations
    task endpoint name changed
    
    updated reducer for receiving tasks

--- src/redux/tasks/operations.js
+++ src/redux/tasks/operations.js
@@ -1,6 +1,5 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
-import { selectUser } from 'redux/authorization/selectors';
 
 axios.defaults.baseURL = 'https://goosetrack-backend-2lsp.onrender.com/api';
 
@@ -8,10 +7,8 @@ export const getTasks = createAsyncThunk(
   'tasks/getAll',
   async (_, thunkAPI) => {
     try {
-      const { id } = selectUser();
-      const res = await axios.get(`/task/${id}`);
-      
-      return res.data;
+      const response = await axios.get(`/tasks`);
+      return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
     }
@@ -22,8 +19,8 @@ export const addTask = createAsyncThunk(
   'tasks/addTask',
   async (newTask, thunkAPI) => {
     try {
-      const res = await axios.post('/task', newTask);
-      return res.data;
+      const response = await axios.post('/tasks', newTask);
+      return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
     }
@@ -34,8 +31,8 @@ export const updateTask = createAsyncThunk(
   'tasks/updateTask',
   async ({ taskId, newTask }, thunkAPI) => {
     try {
-      const res = await axios.patch(`/task/${taskId}`, newTask);
-      return res.data;
+      const response = await axios.patch(`/tasks/${taskId}`, newTask);
+      return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
     }
@@ -46,8 +43,8 @@ export const deleteTask = createAsyncThunk(
   'tasks/deleteTask',
   async (taskId, thunkAPI) => {
     try {
-      const res = await axios.delete(`/task/${taskId}`);
-      return res.data;
+      const response = await axios.delete(`/tasks/${taskId}`);
+      return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
     }
