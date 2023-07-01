0557a75c9c4d397513bb553ea7ff38203de51351
KseniaLF <llf9391@gmail.com>
2023-07-01 22:56:06 +0300

    fix router: add correct header and suspense rendering

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,14 @@
 import 'modern-normalize';
-import { lazy, Suspense, useEffect } from 'react';
+import { lazy, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
-import Header from './header/header';
 import { useDispatch } from 'react-redux';
 import { useAuth } from 'hooks/useAuth';
 import { refreshUser } from 'redux/authorization/operations';
-import { Toaster } from 'react-hot-toast';
 import { RestrictedRoute } from './routes/RestrictedRoute';
 import { PrivateRoute } from './routes/PrivateRoute';
 import { ResendEmainRoute } from './routes/ResendEmailRoute';
+import { Layout } from './Layout';
 
 const Start = lazy(() => import('../pages/Start'));
 const Register = lazy(() => import('../pages/Register'));
@@ -35,15 +34,14 @@ const App = () => {
   return isRefreshing ? (
     <b>Refreshing user...</b>
   ) : (
-    <Suspense fallback={<div>Loading...</div>}>
-      <Routes>
+    <Routes>
+      <Route path="/" element={<Layout />}>
         <Route
-          path="/"
+          index
           element={
             <RestrictedRoute redirectTo="/account" component={<Start />} />
           }
         />
-
         <Route
           path="/register"
           element={
@@ -56,8 +54,8 @@ const App = () => {
             <RestrictedRoute redirectTo="/account" component={<Login />} />
           }
         />
-        <Route path="/verify/:verificationToken" element={<VerifyPage />} />
 
+        <Route path="/verify/:verificationToken" element={<VerifyPage />} />
         <Route
           path="/register/resend-email"
           element={
@@ -70,40 +68,27 @@ const App = () => {
 
         <Route
           path="/account"
-          element={
-            <Suspense fallback={<div>Loading...</div>}>
-              <Header />
-              <PrivateRoute redirectTo="/login" component={<User />} />
-            </Suspense>
-          }
+          element={<PrivateRoute redirectTo="/login" component={<User />} />}
         />
-
         <Route
           path="/statistics"
           element={
-            <>
-              <Header />
-              <PrivateRoute redirectTo="/login" component={<Statistics />} />
-            </>
+            <PrivateRoute redirectTo="/login" component={<Statistics />} />
           }
         />
         <Route
           path="calendar"
           element={
-            <>
-              <Header />
-              <PrivateRoute redirectTo="/login" component={<CalendarPage />} />
-            </>
+            <PrivateRoute redirectTo="/login" component={<CalendarPage />} />
           }
         >
           <Route path="month/:currentDate" element={<ChoosedMonth />} />
           <Route path="day/:currentDay" element={<ChoosedDay />} />
         </Route>
-        <Route path="*" element={<NotFound />} />
-      </Routes>
 
-      <Toaster position="bottom-right" reverseOrder={false} />
-    </Suspense>
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
   );
 };
 
---
+++ src/components/Layout.jsx
@@ -0,0 +1,20 @@
+import { Suspense } from 'react';
+import { Outlet } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import { useAuth } from 'hooks/useAuth';
+import Header from './header/header';
+
+export const Layout = () => {
+  const { isLoggedIn } = useAuth();
+  return (
+    <div>
+      {isLoggedIn && <Header />}
+
+      <Suspense fallback={<div>Loading...</div>}>
+        <Outlet />
+      </Suspense>
+
+      <Toaster position="bottom-right" reverseOrder={false} />
+    </div>
+  );
+};
