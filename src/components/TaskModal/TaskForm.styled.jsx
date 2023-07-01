43cffbf3519902443d214c7323f2f43b38af37c9
Maksym-Plutenko <plutenkom@gmail.com>
2023-07-01 03:57:25 +0300

    add dispatch

--- src/components/TaskModal/TaskForm.styled.jsx
+++ src/components/TaskModal/TaskForm.styled.jsx
@@ -1,4 +1,4 @@
-import { styled } from 'styled-components';
+import  styled  from 'styled-components';
 import { taskColors, buttonColors } from '../../styles/variables';
 
 const Label = styled.label`
