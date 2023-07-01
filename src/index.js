74fa6125c8c73ba7723049021e3a4249d3d42153
OlieHimmars <109204093+OlieHimmars@users.noreply.github.com>
2023-07-01 12:03:22 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { store, persistor } from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
 import { BrowserRouter } from 'react-router-dom';
 
-import { App } from 'components/App';
+import { App } from './components/App';
 
 import GlobalStyle from './styles/GlobalStyle';
 
