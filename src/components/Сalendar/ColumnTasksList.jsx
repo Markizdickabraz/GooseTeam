9a858ae92a255ce5acd8f9520488d0aaa80abc94
Zefristype <Rojdion.a@gmail.com>
2023-07-02 10:08:25 +0200

    link task modal to another button

--- src/components/Сalendar/ColumnTasksList.jsx
+++ src/components/Сalendar/ColumnTasksList.jsx
@@ -2,24 +2,12 @@ import { TasksList } from './ColumnTasksList.styled';
 import { TaskColumnCard } from './TaskColumnCard';
 import { OverlayScrollbarsComponent } from 'overlayscrollbars-react';
 import { useParams } from 'react-router-dom';
-import { format, isSameDay } from 'date-fns';
+import { isSameDay } from 'date-fns';
 import 'overlayscrollbars/overlayscrollbars.css';
 import './Scrollbar.css';
-import TaskModal from '../TaskModal/TaskModal';
 
-export const ColumnTasksList = ({
-  tasks,
-  category,
-  isAddModalOpen,
-  setIsAddModalOpen,
-}) => {
+export const ColumnTasksList = ({ tasks, category }) => {
   const { currentDay } = useParams();
-  const date = format(new Date(currentDay), 'yyyy-MM-dd');
-  const newTask = {
-    date,
-    category,
-  };
-
   return (
     <OverlayScrollbarsComponent
       element="div"
@@ -34,22 +22,11 @@ export const ColumnTasksList = ({
       <TasksList>
         {tasks.map(task => {
           return isSameDay(new Date(currentDay), new Date(task.date)) ? (
-            <TaskColumnCard
-              key={task._id}
-              task={task}
-              category={category}
-            />
+            <TaskColumnCard key={task._id} task={task} category={category} />
           ) : (
             false
           );
         })}
-        {isAddModalOpen && (
-          <TaskModal
-            close={() => setIsAddModalOpen(false)}
-            create={true}
-            task={newTask}
-          />
-        )}
       </TasksList>
     </OverlayScrollbarsComponent>
   );
--- src/components/Сalendar/TasksColumn.jsx
+++ src/components/Сalendar/TasksColumn.jsx
@@ -1,24 +1,38 @@
+import { format } from 'date-fns';
 import { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import TaskModal from '../TaskModal/TaskModal';
 import { AddTaskBtn } from './AddTaskBtn';
 import { ColumnHeadBar } from './ColumnHeadBar';
 import { ColumnTasksList } from './ColumnTasksList';
 import { TaskColumn } from './TasksColumn.styled';
 
 export const TasksColumn = ({ title, tasks, category }) => {
+  const { currentDay } = useParams();
   const [isAddModalOpen, setIsAddModalOpen] = useState(false);
+  const date = format(new Date(currentDay), 'yyyy-MM-dd');
+  const newTask = {
+    date,
+    category,
+  };
 
   return (
     <TaskColumn>
-      <ColumnHeadBar title={title} />
+      <ColumnHeadBar setIsAddModalOpen={setIsAddModalOpen} title={title} />
       <ColumnTasksList
         tasks={tasks}
         category={category}
         isAddModalOpen={isAddModalOpen}
         setIsAddModalOpen={setIsAddModalOpen}
       />
-      <AddTaskBtn
-        setIsAddModalOpen={setIsAddModalOpen}
-      />
+      <AddTaskBtn setIsAddModalOpen={setIsAddModalOpen} />
+      {isAddModalOpen && (
+        <TaskModal
+          close={() => setIsAddModalOpen(false)}
+          create={true}
+          task={newTask}
+        />
+      )}
     </TaskColumn>
   );
 };
